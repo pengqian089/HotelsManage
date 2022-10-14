b0a53ee84bbadce46cf4aa3864a7c51f09403e55
pengqian089 <pengqian089@gmail.com>
2022-10-14 16:15:37 +0800

    start up animation

--- src/HotelsManage/Form1.Designer.cs
+++ src/HotelsManage/Form1.Designer.cs
@@ -46,8 +46,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.blazorWebView1);
+            this.MinimizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.ResumeLayout(false);
 
